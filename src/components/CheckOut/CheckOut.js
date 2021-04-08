88a3f1996abf69cb4951ca0b0a024e724fdf0338
Iftakher99 <19tonmoy99@gmail.com>
2021-04-08 23:43:08 +0600

    email date integrated

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -4,6 +4,9 @@ import Button from "react-bootstrap/Button";
 import { UserContext } from "../../App";
 const CheckOut = () => {
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const [selectedDate, setSelectedDate] = useState({
+    orderDate: new Date(),
+  });
   const [order, setOrder] = useState([]);
   useEffect(() => {
     fetch(
@@ -20,36 +23,48 @@ const CheckOut = () => {
       .then((res) => res.json())
       .then((data) => setOrder(data));
   }, []);
+  const handleSubmit = () => {
+    const newOrder = { ...loggedInUser, ...selectedDate, ...order };
+    fetch("https://protected-lowlands-44213.herokuapp.com/addProducts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newOrder),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div>
       <h1>Check Out</h1>
+      You have: {order.length} Order
       {order.map((order) => (
-        <li key={order._id}></li>
+        <div key={order._id}>
+          <Table striped bordered hover variant='dark'>
+            <thead>
+              <tr>
+                <th>
+                  <h3>{order.name}</h3>
+                </th>
+                <th>Date</th>
+                <th>Price</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{order.orderDate}</td>
+                <td>{order.price}</td>
+                <td>{order.email}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
       ))}
-      <Table striped bordered hover variant='dark'>
-        <thead>
-          <tr>
-            <th>
-              <h3>You have: {order.length} Order</h3>
-            </th>
-            <th>Quantity</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-        </tbody>
-      </Table>
-      <Button variant='success'>Check Out</Button>{" "}
+      <Button onClick={handleSubmit} variant='success'>
+        Check Out
+      </Button>{" "}
     </div>
   );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Products from "../Products/Products";
-
+import "./Home.css";
 const Home = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
@@ -10,24 +10,14 @@ const Home = () => {
         setProducts(result);
       });
   }, []);
-  // const togglerSpinner = (show) =>{
-  //     const spinner = document.getElementById('loading-spinner');
-  //    if(show){
-  //     spinner.classList.remove('d-none');
-  //    }
-  //    else{
-  //     spinner.classList.add('d-none');
-  //    }
-  //   }
 
   return (
     <div className='row'>
-      {/* <div class="d-flex justify-content-center ">
-                <div id="loading-spinner" class="spinner-border text-success  d-none" role="status">
-                    <span class="visually-hidden"></span>
-                </div>
-                togglerSpinner(false)
-            </div> */}
+      {products.length === 0 && (
+        <div class='spinner' style={{ fontSize: "18px" }}>
+          <div class='head'></div>
+        </div>
+      )}
       {products.map((product) => (
         <Products key={product._id} products={product}></Products>
       ))}
